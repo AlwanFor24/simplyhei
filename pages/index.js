699244db04a40e95af08e80131692282ec8fc01b
AlwanFor24 <121307307+AlwanFor24@users.noreply.github.com>
2024-09-19 03:55:54 +0700

    Update nama index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Alwan Maulana Firdaus" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          1227050017 <code>IF UIN Bandung </code>
         </p>
       </main>
 
